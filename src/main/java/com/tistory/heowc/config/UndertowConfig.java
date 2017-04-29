b2db83e4070850a62519fa2806a59f81baf7e473
heo won chul <heowc1992@gmail.com>
2017-04-29 23:30:54 +0900

    modify http2/ssl

--- src/main/java/com/tistory/heowc/config/UndertowConfig.java
+++ src/main/java/com/tistory/heowc/config/UndertowConfig.java
@@ -1,7 +1,7 @@
 package com.tistory.heowc.config;
 
 import io.undertow.UndertowOptions;
-import org.springframework.boot.context.embedded.undertow.UndertowBuilderCustomizer;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +9,11 @@ import org.springframework.context.annotation.Configuration;
 public class UndertowConfig {
 
     @Bean
-    public UndertowBuilderCustomizer createUndertowCustomizerWithHttp2() {
-        return builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true);
+    public UndertowEmbeddedServletContainerFactory createUndertowCustomizerWithHttp2() {
+        UndertowEmbeddedServletContainerFactory factory =
+                new UndertowEmbeddedServletContainerFactory();
+        factory.addBuilderCustomizers(
+                builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true));
+        return factory;
     }
 }
