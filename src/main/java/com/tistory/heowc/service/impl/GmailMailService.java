d22c6fe023775fefe6a3ee2805a8a9b574028fb6
heo won chul <heowc1992@gmail.com>
2017-05-20 17:02:23 +0900

    add async send mail

--- src/main/java/com/tistory/heowc/service/impl/GmailMailService.java
+++ src/main/java/com/tistory/heowc/service/impl/GmailMailService.java
@@ -4,9 +4,11 @@ import com.tistory.heowc.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service("GmailMailService")
+@Async
 public class GmailMailService implements MailService {
 
     @Autowired JavaMailSender javaMailSender;
